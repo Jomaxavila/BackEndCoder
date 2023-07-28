6a97368845ca64cc1fce356ecb41cfe7baaa53f7
Maxi Avila <jomaxavila@gmail.com>
2023-07-27 23:39:23 -0300

    fix restart password

--- src/routes/session.routes.js
+++ src/routes/session.routes.js
@@ -26,7 +26,7 @@ sessionRouter.post('/restartPassword',async(req,res)=>{
   if(!email||!password) return res.status(400).send({status:"error",error:"Incomplete Values"});
   const user = await userModel.findOne({email});
   if(!user) return res.status(404).send({status:"error",error:"Not user found"});
-  const newHashedPassword = createHast(password);
+  const newHashedPassword = createhast(password);
   await userModel.updateOne({_id:user._id},{$set:{password:newHashedPassword}});
 
   res.send({status:"success",message:"Contraseña restaurada"});
